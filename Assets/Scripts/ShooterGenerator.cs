3954fc8b0e892c1cf205f26c1e42fa613caf9d5e
hiraethese <baturovilya123@gmail.com>
2025-02-14 19:18:04 +0100

    Now you can shoot the ball

--- Assets/Scripts/ShooterGenerator.cs
+++ Assets/Scripts/ShooterGenerator.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class ShooterGenerator : MonoBehaviour
 {
+    public float shootForce = 15f;
     public Color[] ballColors = new Color[] {Color.red, Color.green, Color.blue};
+    private Rigidbody rb;
+    private bool isShot = false;
 
     void Start()
     {
         InitializeShooterBall();
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBall();
+        }
+    }
+
     void InitializeShooterBall()
     {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
+
+        rb.useGravity = false;
+        rb.isKinematic = true;
+
         Renderer ballRenderer = GetComponent<Renderer>();
         if (ballRenderer != null && ballColors.Length > 0)
         {
@@ -19,13 +39,34 @@ public class ShooterGenerator : MonoBehaviour
             newMaterial.SetColor("_BaseColor", selectedColor);
             ballRenderer.material = newMaterial;
         }
+    }
 
-        Rigidbody ballRigidbody = GetComponent<Rigidbody>();
-        if (ballRigidbody == null)
+    void ShootBall()
+    {
+        if (rb == null || isShot)
         {
-            ballRigidbody = gameObject.AddComponent<Rigidbody>();
+            return;
         }
-        ballRigidbody.useGravity = true;
-        ballRigidbody.isKinematic = false;
+
+        isShot = true;
+
+        rb.isKinematic = false;
+        rb.useGravity = true;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Vector3 shootDirection;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            shootDirection = (hit.point - transform.position).normalized;
+        }
+        else
+        {
+            shootDirection = Camera.main.transform.forward;
+        }
+
+        rb.AddForce(shootDirection * shootForce, ForceMode.Impulse);
     }
 }
